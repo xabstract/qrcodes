fe51f20b153450a5a731c32475f4f07c10a6f60c
jorge <jorge@gluo.mx>
2016-05-31 21:40:45 -0500

    socket io try
    socket io try

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,35 @@
+
+
+
+
 var resultDiv;
 
 document.addEventListener("deviceready", init, false);
 function init() {
+    
+    var server = require('http').createServer();
+    var io = require('socket.io')(server);
+
+    io.sockets.on('connection', function (socket) {
+        console.log('socket connected');
+
+        socket.on('disconnect', function () {
+            console.log('socket disconnected');
+        });
+
+        socket.emit('text', 'wow. such event. very real time.');
+    });
+
+    server.listen(3000);
+
     document.querySelector("#startScan").addEventListener("touchend", startScan, false);
     resultDiv = document.querySelector("#results");
+    
+    socket.on('connect', function() {
+      socket.on('text', function(text) {
+        alert(text);
+       });
+     });
 }
 
 function startScan() {
